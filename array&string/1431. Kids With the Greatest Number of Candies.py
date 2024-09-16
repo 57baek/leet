c0074eb7966353c93e51e6a455f02dd9710d86c9
HyangMok Baek <57baek@gmail.com>
2024-09-15 23:26:23 -0400

    array&string 1431

--- array&string/1431. Kids With the Greatest Number of Candies.py
+++ array&string/1431. Kids With the Greatest Number of Candies.py
@@ -33,3 +33,5 @@ from typing import List
 class Solution:
     def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
         return [True if (c + extraCandies) >= max(candies) else False for c in candies]
+
+# end
