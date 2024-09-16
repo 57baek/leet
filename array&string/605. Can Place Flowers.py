d7f6259b30d82c5c0fc47f04fa55fc41ab207c04
HyangMok Baek <57baek@gmail.com>
2024-09-15 23:15:10 -0400

    array&string 605

---
+++ array&string/605. Can Place Flowers.py
@@ -0,0 +1,45 @@
+# %%
+
+class Solution:
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        
+        if len(flowerbed) == 0:
+            return False
+
+        tempFlowerbed = flowerbed.copy()  # Avoid modifying the original list
+        count = 0
+        
+        for i in range(len(tempFlowerbed)):
+            
+            # Check for the first spot
+            if i == 0:
+                # Check for the single-value list
+                if len(tempFlowerbed) == 1:
+                    if tempFlowerbed[0] == 0:
+                        tempFlowerbed[0] = 1
+                        count += 1
+                elif tempFlowerbed[0] == 0 and tempFlowerbed[1] == 0:
+                    tempFlowerbed[0] = 1
+                    count += 1
+            
+            # Check for the last spot
+            elif i == len(tempFlowerbed) - 1:
+                if tempFlowerbed[-1] == 0 and tempFlowerbed[-2] == 0:
+                    tempFlowerbed[-1] = 1
+                    count += 1
+            
+            # Check for the middle spots
+            elif tempFlowerbed[i] == 0 and tempFlowerbed[i - 1] == 0 and tempFlowerbed[i + 1] == 0:
+                tempFlowerbed[i] = 1
+                count += 1
+        
+        return count >= n
+            
+# %%
+
+solution = Solution()
+flowerbed = [0,0,0,0,0,1]
+n = 2
+
+result = solution.canPlaceFlowers(flowerbed, n)
+print(result)
