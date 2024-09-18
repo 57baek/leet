9abb0a1297d9613147ef3da3ce89d7c2f931a416
HyangMok Baek <57baek@gmail.com>
2024-09-17 20:13:42 -0400

    array&string 151

--- array&string/151. Reverse Words in a String.py
+++ array&string/151. Reverse Words in a String.py
@@ -35,3 +35,27 @@ s = "     a    ice     cream    "
 result = sol.reverseWords(s)
 print(result)
                 
+# %%
+
+class Solution(object):
+    def reverseWords(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        # Split the string into words
+        words = s.split()
+        
+        # Reverse the list of words
+        reversed_words = words[::-1]
+        
+        # Join the reversed words with spaces
+        reversed_string = ' '.join(reversed_words)
+        
+        return reversed_string
+    
+# %%
+
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        return ' '.join(s.split()[::-1])                
