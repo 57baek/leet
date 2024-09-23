99d32b578f4ff069a9680e73ba903cc2d947805b
HyangMok Baek <57baek@gmail.com>
2024-09-23 12:52:01 -0400

    array_and_string 443

--- array_and_string/443._String_Compression.py
+++ array_and_string/443._String_Compression.py
@@ -1,43 +1,81 @@
 # %%
 
+# from typing import List
+
+# class Solution:
+#     def compress(self, chars: List[str]) -> int:
+#
+#         s = ""
+#         pre_item = ""
+#         count = 0
+#         chars.append("X")
+#
+#         for i in range(len(chars)):
+#
+#             item = chars[i]
+#
+#             # initial
+#             if pre_item == "":
+#                 pre_item = item
+#                 count += 1
+#
+#             elif pre_item == item:
+#                 count += 1
+#
+#             else:
+#
+#                 if count == 1:
+#                     s += pre_item
+#
+#                 elif count > 9:
+#                     s += pre_item
+#                     count_str = str(count)
+#                     for count_char in count_str:
+#                         s += count_char
+#
+#                 pre_item = item
+#                 count = 1
+#
+#         return len(s)
+
+
+# %%
+
 from typing import List
 
 
 class Solution:
     def compress(self, chars: List[str]) -> int:
 
-        s = ""
-        pre_item = ""
-        count = 0
-        chars.append("X")
+        ans = 0
+        i = 0
 
-        for i in range(len(chars)):
+        while i < len(chars):
 
-            item = chars[i]
+            # current letter
+            # reset count to 0
+            letter = chars[i]
+            count = 0
 
-            # initial
-            if pre_item == "":
-                pre_item = item
+            # another loop until different letter shows up
+            # come back to the original index when new letter shows up
+            while i < len(chars) and chars[i] == letter:
                 count += 1
+                i += 1
 
-            elif pre_item == item:
-                count += 1
+            # append new letter to the result
+            # move to the next index
+            chars[ans] = letter
+            ans += 1
 
-            else:
+            # returninig count if greater than 1
+            if count > 1:
 
-                if count == 1:
-                    s += pre_item
+                for c in str(count):
+                    chars[ans] = c
+                    ans += 1
 
-                elif count > 9:
-                    s += pre_item
-                    count_str = str(count)
-                    for count_char in count_str:
-                        s += count_char
-
-                pre_item = item
-                count = 1
-
-        return len(s)
+        return ans
 
 
 # %%
